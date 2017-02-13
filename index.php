b5d31700d21f5728c8e56f17fd0bf9d086f4f41a
AutisticFagetto420 <gm.terkeurs@student.alfa-college.nl>
2017-02-13 13:36:31 +0100

    Startpage design

--- index.php
+++ index.php
@@ -3,20 +3,32 @@
 <HEAD>
 <META content="text/html; charset=utf-8" http-equiv="Content-Type" />
 <LINK href="style.css" rel="stylesheet" type="text/css" />
-<TITLE>Gamepjuh</TITLE>
+<link href="https://fonts.googleapis.com/css?family=Pangolin" rel="stylesheet">
+<TITLE>SRDA</TITLE>
 </HEAD>
 <BODY>
     
-    <h1>Gamepjuh</h1>
+    <h1 class="title">Sybrens riding dildo adventure</h1>
     <div id="test">             <!-- Blok waar de game in komt te staan -->
                                             
 <!--                                            Startpagina-->
                 <div id="naam">
                     <form action="level.php" method="post">                     <!-- Formulier om naam in te vullen --> 
-                    <h2>Naam:</h2>
+                    <h2>Player name:</h2>
                     <input required type="text" name="naam" id="naam"><br>
-                    <input type="submit" name="submit" id="submit" value="Start de game">
+                    <input type="submit" name="submit" id="submit" value="Start game">
                     </form>
+                    <div class="credits_container">
+    					<p class="credits_title">Credits</p>
+    					<ul class="credits">
+	    					<li>Niels van Faassen</li>
+	    					<li>Mark Pals</li>
+					    	<li>Ramon Dubbink</li>
+					    	<li>Lars Hofsink</li>
+					    	<li>Jeroen Mager</li>
+					    	<li>Stijn ter Keurs</li>
+    					</ul>
+    				</div>
                 </div>
     </div>
                                                 
